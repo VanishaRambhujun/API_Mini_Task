092b0bd0d67dd9d55294a2df73eeb8611c87e542
VanishaRambhujun <31880471+VanishaRambhujun@users.noreply.github.com>
2020-07-26 23:18:16 +0200

    Update urls.py
    Fixed typo

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -6,7 +6,7 @@ from .views import SalesAgentViewSet, SalesAgentReportViewSet
 urlpatterns = [
     path('create_agent/<str:name>/<hire_date>/<birthday>/<str:city>', SalesAgentViewSet.as_view({'post': 'create'}),
          name='create_agent'),
-    path('get_all_agents/', SalesAgentViewSet.as_view({'get': 'list'}), name='get_all_reports'),
+    path('get_all_agents/', SalesAgentViewSet.as_view({'get': 'list'}), name='get_all_agents'),
     path('create_report/<str:agent>/<period>/<sale_volume>', SalesAgentReportViewSet.as_view({'post': 'create'}),
          name='create_report'),
     path('get_all_reports/', SalesAgentReportViewSet.as_view({'get': 'list'}), name='get_all_reports')
